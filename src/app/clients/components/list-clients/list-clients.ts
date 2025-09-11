acbcffb9d9dd4881f7981c68b9141a7482f822e6
Diego200509 <diegojijon2005@gmail.com>
2025-09-10 19:24:10 -0500

    Se hizo la creacion de los botones en la lista de clientes

--- src/app/clients/components/list-clients/list-clients.ts
+++ src/app/clients/components/list-clients/list-clients.ts
@@ -3,10 +3,14 @@ import { Container } from '../../../components/container/container';
 import { Table } from '../../../components/table/table';
 import { IClient } from '../../../interfaces/iclient';
 import { IMetaDataColumn } from '../../../interfaces/imeta-data-column';
+import { Paginator } from '../../../components/paginator/paginator';
+import { environment } from '../../../../environments/environment.development';
+import { KeypadButton } from '../../../components/keypad-button/keypad-button';
+import { IKeyPadButton } from '../../../interfaces/ikey-pad-button';
 
 @Component({
   selector: 'app-list-clients',
-  imports: [Container, Table],
+  imports: [Container, Table, Paginator, KeypadButton],
   templateUrl: './list-clients.html',
   styleUrl: './list-clients.css'
 })
@@ -26,6 +30,21 @@ export class ListClients {
     },
     {
       id: '5', dni: '55667788E', firstName: 'Charlie', lastName: 'Carrillo', email: 'charlie@gmail.com'
+    },
+    {
+      id: '6', dni: '12345678A', firstName: 'Juan', lastName: 'Flores', email: 'juan@gmail.com'
+    },
+    {
+      id: '7', dni: '87654321B', firstName: 'Jane', lastName: 'Smith', email: 'jane@gmail.com',
+    },
+    {
+      id: '8', dni: '11223344C', firstName: 'Alice', lastName: 'Paredes', email: 'alice@gmail.com',
+    },
+    {
+      id: '9', dni: '44332211D', firstName: 'Bob', lastName: 'Lopez', email: 'bob@gmail.com'
+    },
+    {
+      id: '10', dni: '55667788E', firstName: 'Charlie', lastName: 'Carrillo', email: 'charlie@gmail.com'
     }
   ];
 
@@ -36,4 +55,27 @@ export class ListClients {
     { field: 'lastName', title: 'APELLIDO' },
     { field: 'email', title: 'CORREO ELECTRONICO' }
   ]
+  records:IClient[] = [];
+  totalRecords:number
+  keypadButtons:IKeyPadButton[] = [
+    {icon:'cloud_download', color:'accent', tooltip:'DESCARGAR', action:'DOWNLOAD'},
+    {icon:'add', color:'primary', tooltip:'AGREGAR', action:'NEW'},
+    {icon:'edit', color:'warn', tooltip:'EDITAR', action:'EDIT'},
+    {icon:'delete', color:'warn', tooltip:'ELIMINAR', action:'DELETE'}
+  ]
+
+  constructor() {
+    this.totalRecords = this.data.length;
+    this.ChangePage(0);
+  }
+
+  ChangePage(page: number) {
+    const pageSize = environment.PAGE_SIZE;
+    const skip = page * pageSize;
+    this.records = this.data.slice(skip, skip + pageSize);
+  }
+
+  doAction(action: string) {
+    console.log('Action:', action);
+  }
 }
--- src/app/components/keypad-button/keypad-button.ts
+++ src/app/components/keypad-button/keypad-button.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+import { IKeyPadButton } from '../../interfaces/ikey-pad-button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-keypad-button',
-  imports: [],
+  imports: [MatButtonModule, MatTooltipModule, MatIconModule],
   templateUrl: './keypad-button.html',
   styleUrl: './keypad-button.css'
 })
 export class KeypadButton {
+  @Input() listButton!:IKeyPadButton[]
+  @Output() onClick:EventEmitter<string> = new EventEmitter<string>();
+  
+  doAction(action:string){
+    this.onClick.emit(action);
+  }
 
 }
